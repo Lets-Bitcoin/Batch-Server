74d89e91bc864a6594e1347467f7544eff6e212b
zestbear <zestbear90@gmail.com>
2024-08-15 22:03:44 +0900

    initial commit

---
+++ src/test/java/com/server/batch/trading/auto/TradingBatchServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.batch.trading.auto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingBatchServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
