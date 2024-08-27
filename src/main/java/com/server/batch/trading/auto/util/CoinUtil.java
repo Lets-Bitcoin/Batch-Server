3d14788fbafd93eb343f0bb68a002a333edcf821
zestbear <zestbear90@gmail.com>
2024-08-27 23:40:39 +0900

    feature: manage coin list

---
+++ src/main/java/com/server/batch/trading/auto/util/CoinUtil.java
@@ -0,0 +1,7 @@
+package com.server.batch.trading.auto.util;
+
+public class CoinUtil {
+    public static final String BITCOIN = "KRW-BTC";
+    public static final String ETHEREUM = "KRW-ETH";
+    public static final String SOLANA = "KRW-SOL";
+}
